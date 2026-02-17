7aca819fa662cbcaaee8932ac15c92f183ea0f7f
MateKuB <sumate.sinmun@gmail.com>
2026-02-17 10:03:30 +0700

    Update configuration for deployment and asset handling. Modified assetPrefix in next.config.ts, added deploy script in package.json, and enhanced GitHub Actions workflow for artifact preparation based on repository type.

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/pdf-signature",
-  assetPrefix: "/pdf-signature/",
+  assetPrefix: "/pdf-signature",
   turbopack: {},
   webpack: (config) => {
     config.resolve.alias.canvas = false;
